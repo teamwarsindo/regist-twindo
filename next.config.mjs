f0cf3b08bd0a2d73fca77d6519a032333850de0e
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-06-19 08:37:59 +0700

    Add async redirects for invite and rules pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  async redirects() {
+    return [
+      {
+        source: '/invite',
+        destination: 'https://discord.gg/NtBBdqUrxe',
+        // Gunakan permanent: false agar jika suatu saat link Discord-mu expired dan kamu ganti link baru, browser pengunjung tidak menyimpan cache link yang lama.
+        permanent: false, 
+      },
+      {
+        source: '/rules',
+        destination: 'https://docs.google.com/document/d/12nvtsG84Us674fte5joAPLY50wlwxHhJhSLew6wn0cw/edit?usp=drivesdk',
+        permanent: false,
+      },
+    ]
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
