8df57052844b8f68e2717a1ca6d3665bef93b720
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-06-23 22:28:15 +0700

    Add launch and close target date constants

---
+++ lib/config.ts
@@ -0,0 +1,9 @@
+// lib/config.ts
+
+// Waktu Buka: 1 Juli 2026, 08:00 WIB (Sesuaikan format ISO 8601)
+export const LAUNCH_TARGET_DATE = "2026-07-01T07:01:26+07:00"
+
+export const LAUNCH_TARGET = new Date(LAUNCH_TARGET_DATE).getTime()
+
+// Waktu Tutup: 14 hari setelah buka
+export const CLOSE_TARGET = LAUNCH_TARGET + (14 * 24 * 60 * 60 * 1000)
