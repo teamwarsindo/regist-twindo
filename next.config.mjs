d89b2ef3d6c1d5668e2fda993454c4a4c805db18
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-06-29 13:34:30 +0700

    Implement redirects for social media links
    Added redirects for Facebook and Instagram.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Tambahkan fungsi redirects di sini
+  async redirects() {
+    return [
+      {
+        source: '/fb',
+        destination: 'https://www.facebook.com/teamwars.id',
+        permanent: true, // Menggunakan HTTP 308 (Permanent Redirect) agar baik untuk SEO
+      },
+      {
+        source: '/ig',
+        destination: 'https://www.instagram.com/teamwarsindonesia',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
