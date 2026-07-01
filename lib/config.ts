68336f1fa85dd15126bc32eb5461e8bdef29e641
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-07-01 14:53:46 +0700

    Update email config to use template literals

--- lib/config.ts
+++ lib/config.ts
@@ -2,8 +2,6 @@
 export const LAUNCH_TARGET_DATE = "2026-07-07T07:07:07+07:00"
 export const LAUNCH_TARGET = new Date(LAUNCH_TARGET_DATE).getTime()
 
-// Waktu Tutup: 14 hari setelah buka
-// export const CLOSE_TARGET = LAUNCH_TARGET + (14 * 24 * 60 * 60 * 1000)
 // Waktu Tutup: 21 Juli 2026, 21:07 WIB
 export const CLOSE_TARGET_DATE = "2026-07-21T07:21:07+07:00"
 export const CLOSE_TARGET = new Date(CLOSE_TARGET_DATE).getTime()
@@ -18,15 +16,15 @@ const DOMAIN = "teamwars.web.id";
 export const EMAIL_CONFIG = {
   from: {
     name: "Team Wars Indonesia",
-    email: "registration@${DOMAIN}",
+    email: `registration@${DOMAIN}`, // Pakai backtick agar variabel terbaca
   },
   // Format untuk digunakan di fungsi pengiriman
-  sender: "Team Wars Indonesia <registration@${DOMAIN}>",
+  sender: `Team Wars Indonesia <registration@${DOMAIN}>`, 
   
   // Daftar tujuan email internal
   to: {
-    finance: "finance@${DOMAIN}",
-    creative: "creative@${DOMAIN}",
-    admin: "admin@${DOMAIN}",
+    finance: `finance@${DOMAIN}`,
+    creative: `creative@${DOMAIN}`,
+    admin: `admin@${DOMAIN}`,
   }
 };
